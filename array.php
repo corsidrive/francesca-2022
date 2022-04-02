06530c46bbca466d2f2844c6f8ab5d7755a0b90a
Roberto Sarasso <sarassoroberto@gmail.com>
2022-04-02 09:59:19 +0200

    Lezione 01

---
+++ array.php
@@ -0,0 +1,54 @@
+<?php
+
+
+$title = "Array in php";
+$colori = ['#f00','#0f0','#00f'];
+$colori = array('#f00','#0f0','#00f');
+$rosso = $colori[0];
+$verde = $colori[1];
+//print_r($colori);
+
+// colori.foreEach()
+// foreach ($colori as $indice => $colore) {
+//     echo "all'indice $indice ho il  <b style='color:$colore'>colore</b><br>";
+// }
+
+
+?>
+
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+<head>
+    <title><?php echo $title ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+    <div class="container mt-3 <?= $colori ?>"  >
+        <h2><?= $title ?></h2>
+        <p>Index Array e Associative Array</p>
+        <ul class="list-group">
+            <!-- inizio loop -->
+            <?php foreach($colori as $indice => $colore) { ?>
+
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Il codice colore è: <?= $colore?>
+                    <span style="background-color:<?=$colors?>!important"  class="badge bg-primary rounded-pill">12</span>
+                </li>
+            
+            <?php } ?>
+
+            <!-- fine loop -->
+            
+        </ul>
+    </div>
+
+</body>
+
+</html>
---
+++ basi.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Basi PHP</title>
+</head>
+
+<body>
+    <?php
+    // const nome = "Mario"
+    $nome = "Mario";
+    $cognome = 'Rossi';
+
+    // frase = "Benvenuto" + nome 
+    // frase = `Benvenuto ${nome}`
+    $frase = "Benvenuto $nome $cognome, come va ?";
+    $altraFrase = 'Benvenuto $nome $cognome, come va ?';
+    $somma = 5 + 5;
+
+    $fraseConPunti = "Benvenuto " . $nome . " " . $cognome . ", come va ?";
+    echo "$frase <br>";
+    echo $altraFrase;
+    /* FINE PHP */
+    // FINE PHP
+    ?>
+    <!--  INIZIO HTML -->
+
+    <h1><?php echo $nome; ?><h1>
+
+            <?php echo "<h1>$nome</h1>" ?>
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo "$nome $cognome" ?></h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+</body>
+
+</html>
